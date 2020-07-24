55f19a77a46962402ac7610064efdb46a966b126
Jarrod Bataille <jtbataille@me.com>
2020-07-24 17:22:00 -0400

    trying to create a working js; added descrition and button to each time slot

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     console.log('time:', time);
 
     // save the value in localStorage as time
-    
+    localStorage.setItem("value", value);
   });
 
   function hourUpdater() {
@@ -25,16 +25,19 @@ $(document).ready(function() {
       console.log("block hour:", blockHour);
 
       // check if we've moved past this time
-      
+      if (blockHour < currentHour) {
       // if the current hour is greater than the block hour
       // then add class "past"
-
+        $(".description").addClass("past");
       // if they are equal
       // then remove class "past" and add class "present"
-
+      } else if (blockHour === currentHour) {
+        $(".description").removeClass("past").addClass("present");
       // else
       // remove class "past", remove class "present", add class "future"
-      
+      } else {
+        $(".description").removeClass("past present").addClass("future");
+      }
     });
   }
 
@@ -42,10 +45,12 @@ $(document).ready(function() {
 
   // set up interval to check if current time needs to be updated
   // which means execute hourUpdater function every 15 seconds
-
+  setInterval(function(){
+    hourUpdater();
+    localStorage.getItem("value");
+ }, 15000);
   // load any saved data from localStorage
-  
-
+  // localStorage.getItem("value");
   // display current day on page
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
 });
