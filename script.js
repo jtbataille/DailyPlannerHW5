58d20644c2344562ead65a71668c8ce93168d4c1
Jarrod Bataille <jtbataille@me.com>
2020-07-26 00:17:01 -0400

    updating comments in js

--- script.js
+++ script.js
@@ -1,55 +1,49 @@
 $(document).ready(function() {
-  // listen for save button clicks
 
+  // set saveBtns to listen to events
   $(".saveBtn").on("click", function() {
-    // get nearby values
+    
     var value = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
     console.log('value:', value);
     console.log('time:', time);
 
-    // save the value in localStorage as time
+    // Saving values into language storage as their time ID
     localStorage.setItem(time, value);
     });
 
   function hourUpdater() {
-    // get current number of hours
+    
     var currentHour = moment().hours();
-    console.log('current hour:', currentHour);
 
-    // loop over time blocks
+    // Create a jQuery loop over all time blocks
     $(".time-block").each(function() {
       var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
-      console.log("block hour:", blockHour);
-
-      // check if we've moved past this time
+      
       if (blockHour < currentHour) {
-      // if the current hour is greater than the block hour
-      // then add class "past"
+        // Hours on calendar taking place before current hour of day, rows will appear grey as determined by class "past"
         $(this).addClass("past");
-      // if they are equal
-      // then remove class "past" and add class "present"
+        // If current hour is the same as that of calendar, row will appear greenas determined by class "present"--removing "past"
       } else if (blockHour === currentHour) {
         $(this).removeClass("past").addClass("present");
-      // else
-      // remove class "past", remove class "present", add class "future"
+        // Hours on calendar taking place after current hour of day, row will appear green as determined by class "future"--removing "past" and "present"
       } else {
         $(this).removeClass("past present").addClass("future");
       }
     });
   }
 
-  // calls the hourUpdater function
+  // Calling the hourUpdater function upon document's initial "ready"
   hourUpdater();
 
-  // set up interval to check if current time needs to be updated
-  // which means execute hourUpdater function every 15 seconds
+  // Interval to fun the hourUpdater function every 15 seconds to adjust for increasing time
   setInterval(function(){
     hourUpdater();
   }, 15000);
-  // load any saved data from localStorage for each hour-div
+  
+  // Retrieve any stored data from localStorage and place it into the correct hour-row
   var hour9 = localStorage.getItem("hour-9");
   $("#hour-9 .description").val(hour9);
 
@@ -77,6 +71,6 @@ $(document).ready(function() {
   var hour17 = localStorage.getItem("hour-17");
   $("#hour-17 .description").val(hour17);
 
-  // display current day on page
+  
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
 });
